70b51421f84d22ca49729553c8c3222853a230ea
Glitch (p5-js-ccfest-rocks-la) <none>
2019-10-21 05:29:33 +0000

    🚓🎺 Checkpoint ./script.js:1792996/242

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function setup() { 
+function setup() {
   createCanvas(400, 400);
-} 
-
-function draw() { 
-  background(220);
-  ellipse(50, )
 }
+
+function draw() {
+  background(220);
+  ellipse(50, 50, 80, 80);
